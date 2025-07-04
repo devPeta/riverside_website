9908715a948378519a05b56556ff6d0b490f3fbd
PeterAny <162056929+devPeta@users.noreply.github.com>
2025-07-04 14:10:17 +0100

    set_up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
 function NavBar() {
 return(
-    <div>
-      <NavBar/>
+    <div className="bg-white shadow-md px-6 py-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold text-gray-800">RIVERSIDE</h1>
+        <div className="flex space-x-6 items-center">
+            <h2 className="text-gray-700 hover:text-blue-600 cursor-pointer">About Us</h2>
+             <h2 className="text-gray-700 hover:text-blue-600 cursor-pointer">Rooms</h2>
+              <h2 className="text-gray-700 hover:text-blue-600 cursor-pointer">Events</h2>
+               <h2 className="text-gray-700 hover:text-blue-600 cursor-pointer">Cuisine</h2>
+                <h2 className="text-gray-700 hover:text-blue-600 cursor-pointer">Contact us</h2>
+                <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">BOOK</button>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ export default {
        "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+        brand: "#2563eb", 
+        darkText: "#1f2937", 
+        lightText: "#374151", 
+        hoverBlue: "#1d4ed8", 
+      },
+    },
   },
   plugins: [],
 }
